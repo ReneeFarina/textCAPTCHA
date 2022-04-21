aa7f6b14b061c99b1407774dc9ec157e15d168db
ReneeFarina <64174705+ReneeFarina@users.noreply.github.com>
2022-04-21 18:03:34 +1000

    edgeDetection first iteration

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 Project for SIT374
 
 pre-processing data for ML models
+
+Contains file for binarisation of images
+* reads in all the images in a folder
+* binarises each image
+* saves off each image into a new folder with a new name
+
+edgeDetection contains research for different methods to remove noise from images
---
+++ edgeDetection.py
@@ -0,0 +1,98 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv.imread('/path/to/folder/za1IB.jpg',0) # will need to change this to a local repo
+ret,thresh1 = cv.threshold(img,127,255,cv.THRESH_BINARY)
+ret,thresh2 = cv.threshold(img,127,255,cv.THRESH_BINARY_INV)
+ret,thresh3 = cv.threshold(img,127,255,cv.THRESH_TRUNC)
+ret,thresh4 = cv.threshold(img,127,255,cv.THRESH_TOZERO)
+ret,thresh5 = cv.threshold(img,127,255,cv.THRESH_TOZERO_INV)
+titles = ['Original Image','BINARY','BINARY_INV','TRUNC','TOZERO','TOZERO_INV']
+images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
+for i in range(6):
+    plt.subplot(2,3,i+1),plt.imshow(images[i],'gray',vmin=0,vmax=255)
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
+plt.show()
+
+img = cv.medianBlur(img,3)
+ret,th1 = cv.threshold(img,143,255,cv.THRESH_BINARY)
+th2 = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_MEAN_C,\
+            cv.THRESH_BINARY,11,2)
+th3 = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,\
+            cv.THRESH_BINARY,11,2)
+titles = ['Original Image', 'Global Thresholding (v = 143)',
+            'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
+images = [img, th1, th2, th3]
+for i in range(4):
+    plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
+plt.show()
+
+
+# global thresholding
+ret1,th1 = cv.threshold(img,143,255,cv.THRESH_BINARY) # orginally set to 127
+# Otsu's thresholding
+ret2,th2 = cv.threshold(img,141,143,cv.THRESH_BINARY+cv.THRESH_OTSU) # was 0,255
+# Otsu's thresholding after Gaussian filtering
+blur = cv.GaussianBlur(img,(5,5),0)
+ret3,th3 = cv.threshold(blur,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+# plot all the images and their histograms
+images = [img, 0, th1,
+          img, 0, th2,
+          blur, 0, th3]
+titles = ['Original Noisy Image','Histogram','Global Thresholding (v=143)', #was 127
+          'Original Noisy Image','Histogram',"Otsu's Thresholding",
+          'Gaussian filtered Image','Histogram',"Otsu's Thresholding"]
+for i in range(3):
+    plt.subplot(3,3,i*3+1),plt.imshow(images[i*3],'gray')
+    plt.title(titles[i*3]), plt.xticks([]), plt.yticks([])
+    plt.subplot(3,3,i*3+2),plt.hist(images[i*3].ravel(),256)
+    plt.title(titles[i*3+1]), plt.xticks([]), plt.yticks([])
+    plt.subplot(3,3,i*3+3),plt.imshow(images[i*3+2],'gray')
+    plt.title(titles[i*3+2]), plt.xticks([]), plt.yticks([])
+plt.show()
+
+
+img = cv.imread('/path/to/folder/za1IB.jpg',0)
+blur = cv.GaussianBlur(img,(5,5),0)
+# find normalized_histogram, and its cumulative distribution function
+hist = cv.calcHist([blur],[0],None,[256],[0,256])
+hist_norm = hist.ravel()/hist.sum()
+Q = hist_norm.cumsum()
+bins = np.arange(256)
+fn_min = np.inf
+thresh = -1
+for i in range(1,256):
+    p1,p2 = np.hsplit(hist_norm,[i]) # probabilities
+    q1,q2 = Q[i],Q[255]-Q[i] # cum sum of classes
+    if q1 < 1.e-6 or q2 < 1.e-6:
+        continue
+    b1,b2 = np.hsplit(bins,[i]) # weights
+    # finding means and variances
+    m1,m2 = np.sum(p1*b1)/q1, np.sum(p2*b2)/q2
+    v1,v2 = np.sum(((b1-m1)**2)*p1)/q1,np.sum(((b2-m2)**2)*p2)/q2
+    # calculates the minimization function
+    fn = v1*q1 + v2*q2
+    if fn < fn_min:
+        fn_min = fn
+        thresh = i
+# find otsu's threshold value with OpenCV function
+ret, otsu = cv.threshold(blur,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+print( "{} {}".format(thresh,ret) )
+
+img2 = cv.imread('/path/to/file/za1IB.jpg',0)
+#(img2,100,200)
+edges = cv.Canny(img2,100,200)
+edges2 = cv.Canny(blur,190,200)
+edges3 = cv.Canny(blur,290,300)
+edges4 = cv.Canny(blur,360,370)
+
+plt.subplot(121),plt.imshow(img2,cmap = 'gray')
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(edges4,cmap = 'gray')
+plt.title('Edge Image 4'), plt.xticks([]), plt.yticks([])
+
+plt.show()
